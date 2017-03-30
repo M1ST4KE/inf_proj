fd9a035569dec92fcbda72349a19e1dc6fe2078a
Kamil Owczarz <kamilo2602@gmail.com>
2017-03-30 15:25:56 +0200

    catalog tree

--- php/delete.php
+++ php/delete.php
@@ -5,23 +5,23 @@ if (!isset($_SESSION['logged']) && $_SESSION['logged'] == false) {
     exit();
 } else
 
-    require("connect.php");
+    require("connect.php"); //zwraca zmienną $connection
 
 $username = $_SESSION['username'];
-$query = "SELECT id, prev_lvl FROM `user` WHERE username ='$username'";
+$query = "SELECT `id`, `prev_lvl` FROM `user` WHERE `username` ='$username'";
 $prev = $connection->query($query);
 $prev = $prev->fetch_assoc();
 
 $id = $_GET['id'];
 
-$query = "SELECT prev_lvl FROM `users` WHERE id ='$id'";
+$query = "SELECT `prev_lvl` FROM `users` WHERE `id` ='$id'";
 $pr = $connection->query($query);
 $pr = $pr->fetch_assoc();
 var_dump($id);
 
 if ($prev['prev_lvl'] - $pr['prev_lvl'] > 0 && $id != $prev['id']) {
     var_dump($id);
-    $sql = "DELETE FROM `user` WHERE id ='$id'";
+    $sql = "DELETE FROM `user` WHERE `id` ='$id'";
     $connection->query($sql);
 } else {
 
--- php/edit.php
+++ php/edit.php
@@ -4,4 +4,21 @@ if (!isset($_SESSION['logged']) && $_SESSION['logged'] == false) {
     header("Location: ../index.php");
     exit();
 }
+
+require("connect.php");
+
+$username = $_SESSION['username'];
+$query = "SELECT id, prev_lvl FROM `user` WHERE username ='$username'";
+$prev = $connection->query($query);
+$prev = $prev->fetch_assoc();
+
+$id = $_GET['id'];
+
+$query = "SELECT prev_lvl FROM `users` WHERE id ='$id'";
+$pr = $connection->query($query);
+$pr = $pr->fetch_assoc();
+
+if ($prev['prev_lvl'] - $pr['prev_lvl'] > 0 || $id != $prev['id']) {
+    var_dump($pr);
+}
 ?>
