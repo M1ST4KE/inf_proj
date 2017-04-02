f022a1584e8a2e6180c1f0b9217a613818d5d0c6
Kamil Owczarz <kamilo2602@gmail.com>
2017-04-02 19:46:45 +0200

    edit update

--- php/edit.php
+++ php/edit.php
@@ -89,17 +89,21 @@ if (!($prev['prev_lvl'] - $pr['prev_lvl'] >= 0 || $id != $prev['id'])) {
 ?>
     <div class="container">
         <?php
-        $query = "SELECT * FROM `user` WHERE `id`=" . $_GET['id_gory'] . ';';
+        $query = "SELECT * FROM `user` WHERE `id`=" . $_GET['id'] . ';';
         $result = $connection->query($query);
         $row = $result->fetch_array();
         echo '<form method="post">';
-        echo '<input type="hidden" name="id_gory" value="' . $_GET['id_gory'] . '" />';
-        echo '<label for="nazwa">Nazwa</label>';
+        echo '<input type="hidden" name="id" value="' . $_GET['id'] . '" />';
+        echo '<label for="nazwa">Nazwa </label>';
         echo '<input type="text" name="nazwa" placeholder="' . $row['username'] . '" value="' . $row['username'] . '"/>';
-        echo '<label for="nazwa">Nazwa</label>';
-        echo '<input type="text" name="nazwa" placeholder="' . $row['email'] . '" value="' . $row['email'] . '"/>';
-        echo '<label for="nazwa">Nazwa</label>';
-        echo '<input type="text" name="nazwa" value="' . md5($row['password']) . '"/>';
+        echo '<label for="email">Email </label>';
+        echo '<input type="text" name="email" placeholder="' . $row['email'] . '" value="' . $row['email'] . '"/>';
+        echo '<label for="haslo">Hasło </label>';
+        echo '<input type="text" name="haslo" value="' . md5($row['password']) . '" />';
+        if ($prev['prev_lvl'] >= 3) {
+            echo '<label for="poz">';
+            echo '<input type="text" name="poz" value="' . $row('prev_lvl') . '" placeholder="' . $row['email'] . '"/>';
+        }
         echo '<button type="submit" class="btn btn-success">Zatwierdź!</button>';
         echo '<a type="submit" href="../users.php" role="button" class="btn btn-primary">Zatwierdź i wróć</a>';
         echo "</form>";
