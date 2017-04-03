3625df53c6512823745af27b76e629cbd1cfd728
Kamil Owczarz <kamilo2602@gmail.com>
2017-04-03 19:32:18 +0200

    kosmatyczne poprawki vol.3

--- php/edit.php
+++ php/edit.php
@@ -8,9 +8,16 @@
 <!--[if gt IE 8]><!-->
 <html class="no-js" lang=""> <!--<![endif]-->
 <head>
+    <?php
+    session_start();
+    if (!isset($_SESSION['logged']) && $_SESSION['logged'] == false) {
+        header("Location: index.php");
+        exit();
+    }
+    ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title></title>
+    <title>Edytuj <?php echo($_SESSION['username']); ?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -36,13 +43,7 @@
     <script src="../js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
     <script src="../js/main.js"></script>
 </head>
-<?php
-session_start();
-if (!isset($_SESSION['logged']) && $_SESSION['logged'] == false) {
-    header("Location: index.php");
-    exit();
-}
-?>
+
 <body>
 <!--[if lt IE 8]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
--- session.php
+++ session.php
@@ -8,9 +8,16 @@
 <!--[if gt IE 8]><!-->
 <html class="no-js" lang=""> <!--<![endif]-->
 <head>
+    <?php
+    session_start();
+    if (!isset($_SESSION['logged']) && $_SESSION['logged'] == false) {
+        header("Location: index.php");
+        exit();
+    }
+    ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title></title>
+    <title><?php echo($_SESSION['username']); ?></title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -37,13 +44,7 @@
     <script src="js/main.js"></script>
 </head>
 <body>
-<?php
-session_start();
-if (!isset($_SESSION['logged']) && $_SESSION['logged'] == false) {
-    header("Location: index.php");
-    exit();
-}
-?>
+
 <!--[if lt IE 8]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade
     your browser</a> to improve your experience.</p>
